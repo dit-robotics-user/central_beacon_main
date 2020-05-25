f4106e8364d85bd464261bedd5466266bc66831b
WangMahua <whole9177j53@gmail.com>
2020-05-25 20:20:01 +0800

    0525 fix format

--- src/cb_main.cpp
+++ src/cb_main.cpp
@@ -11,6 +11,17 @@
 #include <cmath>
 #include <stdlib.h>
 
+//availible form 
+//N S : 0 1 
+//color green red : 0 1 
+//cup color : 0 0 0 0 0
+//transfer form 
+//0 : don't eat
+//1 : eat data
+//2 : finish 
+//3 : eat garbage
+
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "cb_main");
@@ -21,14 +32,60 @@ int main(int argc, char **argv)
     central_beacon_main::ns srv_ns;
     double begin_time =ros::Time::now().toSec();
     int count = 0;
-ros::Rate loop_rate(1);
-
+    int cup_suck = 0;
+    int ns_suck = 0;
+    srv_cup.request.OUO = 0;
+    srv_ns.request.OAO = 0;
+    
+  
+    ros::Rate loop_rate(1);
 
     while(ros::ok){
         double clustering_time = ros::Time::now().toSec () - begin_time; 
-        srv_cup.request.OUO = 1;
-        srv_ns.request.OAO = 2;
-        if(count == 0 ){
+
+        if(clustering_time>0.0001){
+            srv_cup.request.OUO=1;
+        }
+        if(clustering_time>30){
+            srv_ns.request.OAO=1;
+        }
+        if(srv_cup.request.OUO==1){
+            if(client_cup.call(srv_cup)){
+                for(int i;i<5;i++){
+                    if(srv_cup.response.CupResult[i]!=0 && srv_cup.response.CupResult[i]!=1 ){
+                        cup_suck = 1 ;     
+                    }else{
+                        srv_cup.request.OUO = 2 ;//finish
+                    } 
+                }
+            }   
+        }
+        if(cup_suck == 1 ){
+            srv_cup.request.OUO = 3;
+        }
+
+
+
+        if(srv_ns.request.OAO==1){
+            if(client_ns.call(srv_ns)){
+                if(srv_ns.response.ns !=0 && srv_ns.response.ns!=1 ){
+                    ns_suck = 1 ;     
+                }else{
+                    srv_ns.request.OAO = 2 ;//finish 
+                } 
+            }   
+        }
+        if(cup_suck == 1 ){
+            srv_cup.request.OUO = 3;
+        }
+
+
+
+
+
+
+
+        if(count == 0 && clustering_time>4){
             if (client_cup.call(srv_cup)){
                 ROS_INFO("Sum: %ld", (long int)srv_cup.response.CupResult[0]);
                 ROS_INFO("Sum: %ld", (long int)srv_cup.response.CupResult[1]);
